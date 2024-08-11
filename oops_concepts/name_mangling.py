cffc1141c09aa0d7d6fcffd666e5a648aee47e49
LuciAkirami <ajsubscriber001@gmail.com>
2024-08-11 15:59:27 +0530

    edited name_mangling

--- oops_concepts/name_mangling.py
+++ oops_concepts/name_mangling.py
@@ -195,3 +195,17 @@ This example shows how name mangling helps prevent accidental access or modifica
 """
 
 print(f"\n{explanation}")
+
+
+"""
+Extra Information
+“Private” instance variables that cannot be accessed except from inside an object, don’t exist in Python. 
+However, there is a convention that is followed by most Python code: a name prefixed with an underscore (e.g. _spam) should be treated 
+as a non-public part of the API (whether it is a function, a method or a data member). 
+It should be considered an implementation detail and subject to change without notice.
+
+Since there is a valid use-case for class-private members (namely to avoid name clashes of names with names defined by subclasses), 
+there is limited support for such a mechanism, called name mangling. Any identifier of the form __spam (at least two leading underscores, 
+at most one trailing underscore) is textually replaced with _classname__spam, where classname is the current class name with leading underscore(s) 
+stripped. This mangling is done without regard to the syntactic position of the identifier, as long as it occurs within the definition of a class.
+"""
