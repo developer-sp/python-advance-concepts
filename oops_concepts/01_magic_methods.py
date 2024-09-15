43c2809ff802660e76ee3abcab128da520d37fd3
Lucifer Akirami <110820999+LuciAkirami@users.noreply.github.com>
2024-09-15 14:01:36 +0000

    created magic_methods.py in oops

---
+++ oops_concepts/01_magic_methods.py
@@ -0,0 +1,5 @@
+'''
+Magic methods are methods that have double underscores at the beginning and end of their names. These are called dunder methods too
+
+On a side note, the word “Dunder” is short for Double Underscore.
+'''
